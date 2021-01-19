75f333d8e90e5429110e6fb949d3a38716c94fbf
2222041633 <207569820@qq.com>
2021-01-19 16:04:38 +0800

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <frame-bar title="bbs.125.la"></frame-bar>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import FrameBar from "./FrameBar.vue";
+export default defineComponent({
+  name: "Home",
+  components: {
+    FrameBar,
+  },
+});
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import installElementPlus from './plugins/element'
-
+import './electron/global'
 const app = createApp(App)
 installElementPlus(app)
-app.use(store).use(router).mount('#app')
+app.use(router).mount('#app')
